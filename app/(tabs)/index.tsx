ebcf3b11bccaf4bf9a2014fbe17e810c0a324eb8
ayebanua <ayebanua@algocipherquantitative.com>
2025-06-23 17:47:24 +0100

    Merge branch 'master' of https://github.com/OtieteAyebanua/skillgaplastbuild

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,15 +14,13 @@ import StepIndicator from "./auth/StepIndicator";
 import SplashScreen from "./splashScreen";
 
 const Onboarding = () => {
-  router.push("/(tabs)/components/personalizedSettings");
-
   const step1Img = require("../../assets/images/onboarding-step1.png");
   const step2Img = require("../../assets/images/onboarding-step2.png");
   const step3Img = require("../../assets/images/onboarding-step3.png");
   const [step, setStep] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
   const [isFirstTime, setIsFirstTime] = useState(true);
-  Router.push("/(tabs)/components/personalizedSettings");
+  Router.push('/(tabs)/components/personalizedSettings')
   useEffect(() => {
     checkForFirstTimeInstallation();
   }, []);
@@ -53,8 +51,9 @@ const Onboarding = () => {
     if (step < 3) {
       setStep(step + 1);
     } else {
-      await Device.register();
-      Router.push("/auth/auth-home");
+     // await Device.register();
+   //   Router.push("/auth/auth-home");
+     router.push('/(tabs)/components/personalizedSettings')
     }
   };
 
